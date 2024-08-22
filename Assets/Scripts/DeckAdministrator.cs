b2b1a0898c6c29f9a6c372ddc0011dbb04867f71
EmoriDev <48255514+EmoriDev@users.noreply.github.com>
2024-08-22 00:40:24 -0300

    set hand

--- Assets/Scripts/DeckAdministrator.cs
+++ Assets/Scripts/DeckAdministrator.cs
@@ -7,28 +7,33 @@ public class DeckAdministrator : MonoBehaviour
     [SerializeField] private List<Card> Deck = new List<Card>();
     [SerializeField] private Hand mainHand;
     [SerializeField] private GameObject Card;
+
+    public static DeckAdministrator instance;
     // Start is called before the first frame update
     void Start()
     {
-        FillHand(mainHand.Get_HandMaxSize());
+        //FillHand(mainHand.Get_HandMaxSize());
+        instance = this;
+    }
+    public void FillDeck(Card card)
+    {
+        Deck.Add(card);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     //Fill
     public void FillHand(int HandSize)
     {
+        if (mainHand.Get_CardOnHandNum() > 0)
+        {
+            mainHand.SendHandBackToDeck();
+        }
         for(int i = 0; i< HandSize; i++)
         {
-            // set card on script
+            // set card
             int index = Random.Range(0, Deck.Count);
-            mainHand.Set_Hand(Deck[index]);
-            // set card on screen
             GameObject obj = Instantiate(Card, new Vector3(-7.5f + (3.75f * i), -3, 0), Quaternion.identity);
             obj.GetComponentInChildren<TMP_Text>().text = Deck[index].Get_CardName();
+            mainHand.Set_Hand(Deck[index], obj);
             Deck.RemoveAt(index);
         }
     }
--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    [SerializeField] private List<Card> HandDeck = new List<Card>();
-    [SerializeField] private int HandStartSize = 5;
-    public void Set_Hand(Card card)
+    struct HandStruct
     {
-        HandDeck.Add(card);
+        [SerializeField] private Card InternalCard;
+        [SerializeField] private GameObject VisualCard;
+
+        public Card Get_InternalCard()
+        {
+            return InternalCard;
+        }
+        public GameObject Get_VisualCard()
+        {
+            return VisualCard;
+        }
+
+        public HandStruct(Card c, GameObject v)
+        {
+            InternalCard = c;
+            VisualCard = v;
+        }
+    }
+    [SerializeField] private List<HandStruct> HandDeck = new List<HandStruct>();
+    [SerializeField] private int HandStartSize = 5;
+    
+    public void Set_Hand(Card card, GameObject visual)
+    {
+        HandDeck.Add(new HandStruct(card, visual));
     }
     public int Get_HandMaxSize()
     {
         return HandStartSize;
+    }
+    public int Get_CardOnHandNum()
+    {
+        return HandDeck.Count;
+    }
+    public void SendHandBackToDeck()
+    {
+        foreach(HandStruct card in HandDeck)
+        {
+            DeckAdministrator.instance.FillDeck(card.Get_InternalCard());
+            Destroy(card.Get_VisualCard());
+        }
+        HandDeck.Clear();
     }
 }
